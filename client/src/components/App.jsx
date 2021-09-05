c655352a024fac598b1ca65c37f4a57c436de0f5
Cassandra Barragan <cassbarragan@gmail.com>
2021-09-05 15:59:45 -0700

    dropdown rendering without duplicates

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -21,11 +21,75 @@ class App extends React.Component {
   getData(){
     axios.get('/get')
     .then((response) => {
-      console.log('data from axios on front end:', response.data);
+      // console.log('data from axios on front end:', response.data);
       this.setState({list:response.data})
     })
     .catch(err => console.log('ERROR:', err));
   }
+
+  render() {
+    const {list} = this.state // {list:[{},...]}
+    const typeArr = list?.map(({type}) => type );
+    const types = [...new Set(typeArr)];
+
+    // console.log("list:",list)
+    return(
+      <div>
+        <h1>Fullstack Pokedex!</h1>
+        <button>Show All</button>
+        <select id="types">
+          {types?.map((type,idx) => {
+                // console.log("type",type)
+              return(
+                <option key={idx}>{type}</option>
+              )
+            })
+          }
+        </select>
+        <List list={this.state.list}/>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   //another way to do this but with destructuring:
   // getData(){
@@ -36,25 +100,3 @@ class App extends React.Component {
   //   })
   //   .catch(err => console.log('ERROR:', err));
   // }
-
-  render() {
-    return(
-      <div>
-        <h1>Fullstack Pokedex!</h1>
-        <button>Show All</button>
-        <select id="types">
-          <option>Sort by Type</option>
-          <option>Grass</option>
-          <option>Fire</option>
-          <option>Water</option>
-        </select>
-        <List list={this.state.list}/>
-      </div>
-    )
-  }
-}
-
-
-export default App;
-
-
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 var List = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div>
       {props.list.map((item) => {
-        // console.log("item in List.js",item)
-        console.log('itemimage', item.img);
           return (
             <div>
             <h3>{item.name}</h3>
-            <img src={`${item.img}`} />
+            <img src={item.img} />
             </div>
             )}
       )}
